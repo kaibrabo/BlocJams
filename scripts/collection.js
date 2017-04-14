3119a9dc39be92f31914b83266e7b8ef71b6d664
Kai Brabo <kaibrabo@gmail.com>
2017-04-14 13:14:53 -0700

    cp 17 jquery views: completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,30 +1,38 @@
-var collectionItemTemplate = 
-	'<div class="collection-album-container column fourth">'
-   + '  <img src="assets/images/album_covers/01.png"/>'
-   + '  <div class="collection-album-info caption">'
-   + '    <p>'
-   + '      <a class="album-name" href="album.html"> The Colors </a>'
-   + '      <br/>'
-   + '      <a href="album.html"> Pablo Picasso </a>'
-   + '      <br/>'
-   + '      X songs'
-   + '      <br/>'
-   + '    </p>'
-   + '  </div>'
-   + '</div>'
-   ;
+var buildCollectionItemTemplate = function () {
+	
+	var template = 
+			'<div class="collection-album-container column fourth">'
+		+ '  <img src="assets/images/album_covers/01.png"/>'
+		+ '  <div class="collection-album-info caption">'
+		+ '    <p>'
+		+ '      <a class="album-name" href="album.html"> The Colors </a>'
+		+ '      <br/>'
+		+ '      <a href="album.html"> Pablo Picasso </a>'
+		+ '      <br/>'
+		+ '      X songs'
+		+ '      <br/>'
+		+ '    </p>'
+		+ '  </div>'
+		+ '</div>'
+		;
+	
+		return $(template);
+		//function returns the markup string as a jQuery object, which we'll call a jQuery template. 
+};
 
-window.onload = function() {
+
+$(window).on('load', function(){
 	
-	var collectionContainer = document.getElementsByClassName('album-covers')[0];
-	//we select the first (and only, as we've designed it) element with an album-covers class name
+	var $collectionContainer = $('.album-covers');
 	
-	collectionContainer.innerHTML = '';
-	//We assign an empty string to collectionContainer's innerHTML property to clear its content. This ensures we're working with a clean slate before we insert content with JavaScript.
+	$collectionContainer.empty();
 	
 	for (var i = 0; i < 12; i++){
-		collectionContainer.innerHTML += collectionItemTemplate;
+		
+		var $newThumbnail = buildCollectionItemTemplate();
+		
+		$collectionContainer.append($newThumbnail);
 	}
-	//that inserts 12 albums using the += operator, which appends content to strings. Each loop adds the contents of  collectionItemTemplate (the template) to the innerHTML of  collectionContainer, thereby generating the albums that display on the collection page.
 	
-}
+});
+	
